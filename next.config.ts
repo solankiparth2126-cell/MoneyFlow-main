8f01a3ba8548babfe073cb4527d283aa9475f953
solankiparth2126-cell <solankiparth2126@gmail.com>
2026-03-15 12:24:57 +0530

    Add API rewrites to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://moneyflowapi.runasp.net/api/:path*',
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
